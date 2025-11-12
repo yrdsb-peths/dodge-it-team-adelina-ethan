d87c8887e7726b17b323781d89941fadbe799e1a
adelinalyw <adelina.lyw@gmail.com>
2025-11-12 14:19:41 -0500

    added apple

---
+++ Apple.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Apple extends Actor
+{
+    public Apple() {
+        setImage ("images/apple2.png");
+    }
+    public void act()
+    {
+        move (-10);
+        if(getX() <= 0){
+            resetApple();
+        }
+    }
+    public void resetApple() {
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0 ){
+            setLocation(600, 150);
+        } else{
+            setLocation(600, 250);
+        }
+    }
+}
--- Banana.java
+++ Banana.java
@@ -24,7 +24,7 @@ public class Banana extends Actor
     public void resetBanana() {
         int num = Greenfoot.getRandomNumber(2);
         if(num == 0 ){
-            setLocation(600, 300);
+            setLocation(600, 100);
         } else{
             setLocation(600, 300);
         }
