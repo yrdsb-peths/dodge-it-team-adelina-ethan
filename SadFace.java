8aba6516df85aba73636425440f06b700818da75
adelinalyw <adelina.lyw@gmail.com>
2025-11-12 14:02:49 -0500

    added end game symbol

---
+++ SadFace.java
@@ -0,0 +1,12 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class SadFace extends Actor
+{
+    public SadFace() {
+        setImage ("images/smiley3.png");
+    }
+    public void act()
+    {
+        
+    }
+}
